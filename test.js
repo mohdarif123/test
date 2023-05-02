823c93bbaa84470f8588d411f07ab234933eb54d
mohd arif <mohdarifmansoori6063@gmail.com>
2023-05-02 18:37:08 +0530

    Revert "4"
    This reverts commit 5908df1739e83de9549b21a53367c93b6f43ac03.

--- test.js
+++ test.js
@@ -2,7 +2,6 @@ const arr = [2,3,3,4,4];
 
 const ar1 = [2,3,3,4,4];
 const ar3 = [2,3,3,4,4];
-const ar4 = [2,3,3,4,4];
 const result = arr.map((item,index)=>{
 const y = arr.indexOf(item) !== index;
 console.log(y)
