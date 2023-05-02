4ac9e33e1c1b421fed995425ccf8885967cb0ec0
mohd arif <mohdarifmansoori6063@gmail.com>
2023-05-02 18:54:49 +0530

    added

---
+++ gitDetails.js
@@ -0,0 +1,45 @@
+# test
+This git repositary from basic to advance
+and it will help for beginner guys
+
+
+1-> GIT -> git is a version control system. and git maintain version history so we can revert to anyof previous state of your code.
+
+2->without git Problem :-
+   1. if multiple user want work on the same project so at that condition we need to send project file throught zip or pendrive etc.
+   2. if we save code on local so if hard disk failure or computer crash so code will detroy.
+   
+   -------Command------
+ 3->Git clone :- it will create or copy of project into local machine.
+                      git clone url
+                      git clone url
+ 4-> git status :- see all the changes 
+                  git status
+                 git status
+ 5-> Staging:- its area which contain the file which ready to commit.
+ 6-> git add . :- use to add all changes goes into staging area
+                      git add .
+                      git add .
+  7-> git commit -m "message given by developer" :-commit the files/changes to the local repository
+                    git commit -m "new functionality added"
+                   git commit -m "new functionality added"
+  8-> git log:- show all commit history
+                   git log
+        
+   9-> git push:- git push will push committe changes to remote server(git)
+                     git push
+         git push
+   10-> git difftool HEAD :- it will show the previous change and latest changes
+                      git difftool HEAD
+                      git difftool HEAD
+   11-> git reset :-This means that when you want to undo the changes in multiple commits, git reset may be more useful but should mostly be used only when changes have not been made public, while git revert can be used when only a specific commit needs to be undone and can be used even when the changes have been made public.
+
+         git revert commitId
+   
+   11-> undo local change :- change your local changes
+                       git checkout .
+
+   12-> undo after commit code :- we can undo the code after commit
+                 git revert commitId
+
+                        
